9aa51d6682c4a8ef9affe206d9aa203214f7976a
Rohitash Kator <rohitash.kator@talentica.com>
2025-09-15 17:21:22 +0530

    Starting Angular

--- angular/angular-components-and-templates-deep-dive/src/app/components/shared/button/button.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'button[appButton], a[appButton]',
-  imports: [],
-  templateUrl: './button.component.html',
-  styleUrl: './button.component.css',
-})
-export class ButtonComponent {}
