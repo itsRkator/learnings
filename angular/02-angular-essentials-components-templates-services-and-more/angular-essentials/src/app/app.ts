a347ca443ff194dc804e7fe5db5a2cd8f23c1a6d
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-10 13:43:10 +0530

    Outputs and emitting data using decoratiors

--- angular/02-angular-essentials-components-templates-services-and-more/angular-essentials/src/app/app.ts
+++ angular/02-angular-essentials-components-templates-services-and-more/angular-essentials/src/app/app.ts
@@ -1,15 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./header/header";
-import { User } from "./user/user";
+import { Header } from './header/header';
+import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, User],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log(id);
+  }
 }
--- angular/02-angular-essentials-components-templates-services-and-more/angular-essentials/src/app/user/user.ts
+++ angular/02-angular-essentials-components-templates-services-and-more/angular-essentials/src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input, Input } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,18 +9,22 @@ import { Component, computed, input, Input } from '@angular/core';
 })
 export class User {
   // Accepting inputs using Decorators
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar();
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   // Accepting inputs using Signals (Input signals are readonly)
-  avatar = input.required<string>();
-  name = input.required<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  imagePath = computed(() => 'assets/users/' + this.avatar());
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
