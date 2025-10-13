43511df8029c6147c7650a709e5b3de584732a99
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-13 16:57:52 +0530

    Adding Routes

--- angular/03-angular-essentials-working-with-modules/angular-essentials-modules/src/app/app.module.ts
+++ angular/03-angular-essentials-working-with-modules/angular-essentials-modules/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
@@ -8,10 +9,16 @@ import { UserModule } from './user/user.module';
 import { CardModule } from './shared/card/card.module';
 import { TasksModule } from './tasks/tasks.module';
 
-
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([]), HeaderModule, UserModule, CardModule, TasksModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HeaderModule,
+    UserModule,
+    CardModule,
+    TasksModule,
+  ],
 })
 export class AppModule {}
