a4437f1380e040f2ae6f4b43f4b39b793e401846
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-15 15:06:12 +0530

    Binding properties to host elements using HostBinding decorator and Listening to  events at the host element using HostListening decorator

--- angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/shared/control/control.ts
+++ angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/shared/control/control.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,23 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   // host property add the key value pairs as properties to the host element
   host: {
-    class: 'control'
-  }
+    class: 'control',
+    // event binding
+    '(click)': 'onClick()',
+  },
 })
 export class Control {
+  // HostBinding should be omitted
+  // @HostBinding('class') className = 'control';
+
+  // HostListener is a cleaner and better approach for event binding to the host
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked');
+  // }
+
   label = input.required<string>();
+
+  onClick() {
+    console.log('Clicked');
+  }
 }
