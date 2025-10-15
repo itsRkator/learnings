306c3febb376c5b7f094c3d568c2248eeb61a46a
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-15 12:57:49 +0530

    Multi-Element Custom component selector and content projection

--- angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/shared/button/button.ts
+++ angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/shared/button/button.ts
@@ -1,7 +1,7 @@
 import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]', // Component Selector (attribute selector)
+  selector: 'button[appButton], a[appButton]', // Component Selector (attribute selector), multi-selector (for button and anchor tags)
   imports: [],
   templateUrl: './button.html',
   styleUrl: './button.css',
