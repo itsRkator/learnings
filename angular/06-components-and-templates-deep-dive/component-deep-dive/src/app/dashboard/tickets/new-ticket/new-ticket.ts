0c0360f89fd7e87d4f18fc00144201e29c86c9a5
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-15 18:57:36 +0530

    Accessing form data using template variables

--- angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { Button } from '../../../shared/button/button';
-import { Control } from "../../../shared/control/control";
+import { Control } from '../../../shared/control/control';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [Button, Control],
+  imports: [Button, Control, FormsModule],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css',
 })
-export class NewTicket {}
+export class NewTicket {
+  onSubmit(title: HTMLInputElement, request: HTMLTextAreaElement): void {
+    console.dir(title.value);
+    console.dir(request.value);
+  }
+}
