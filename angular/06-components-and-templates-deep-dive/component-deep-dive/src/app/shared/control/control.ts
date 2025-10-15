726c998f9e06db68b92c967925adcb44d08284ff
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-15 14:48:43 +0530

    Adding properties to the host element using the host propery of the component decorator

--- angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/shared/control/control.ts
+++ angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/shared/control/control.ts
@@ -6,6 +6,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.html',
   styleUrl: './control.css',
   encapsulation: ViewEncapsulation.None,
+  // host property add the key value pairs as properties to the host element
+  host: {
+    class: 'control'
+  }
 })
 export class Control {
   label = input.required<string>();
