b6473fc14675bd71d91460804c3479c2145f45a6
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-15 14:57:22 +0530

    When to rely and when to not rely on host element

--- angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.ts
+++ angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.ts
@@ -1,10 +1,13 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   imports: [],
   templateUrl: './dashboard-item.html',
   styleUrl: './dashboard-item.css',
+  // host: {
+  //   class: 'dashboard-item',
+  // },
 })
 export class DashboardItem {
   image = input.required<{ src: string; alt: string }>();
