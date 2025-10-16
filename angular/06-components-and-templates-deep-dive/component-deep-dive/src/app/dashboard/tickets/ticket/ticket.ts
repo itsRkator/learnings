fd503eebc3688ceb3c732f705401efa314b8d6bf
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-16 13:42:17 +0530

    Passing data across the components - Component Commmunication and state management

--- angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/dashboard/tickets/ticket/ticket.ts
+++ angular/06-components-and-templates-deep-dive/component-deep-dive/src/app/dashboard/tickets/ticket/ticket.ts
@@ -1,13 +1,34 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+  signal,
+} from '@angular/core';
 import { type Ticket as TicketType } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   imports: [],
   templateUrl: './ticket.html',
-  styleUrl: './ticket.css'
+  styleUrl: './ticket.css',
 })
 export class Ticket {
-  @Input({required: true}) ticket!: TicketType;
+  // @Input({required: true}) ticket!: TicketType;
+  // @Output() complete = new EventEmitter<void>();
 
+  ticket = input.required<TicketType>();
+  detailsVisible = signal<boolean>(false);
+  close = output<string>();
+
+  onToggleDetails() {
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit(this.ticket().id);
+  }
 }
