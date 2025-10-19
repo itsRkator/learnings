29f93d6981b3262cd284f2db72820f3374e49b70
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-19 21:06:27 +0530

    Different ways to inject and what to do and what not while injecting

--- angular/09-understanding-services-and-dependency-injection-deep-dive/services-and-dependency-injection-deep-dive/src/app/logging.ts
+++ angular/09-understanding-services-and-dependency-injection-deep-dive/services-and-dependency-injection-deep-dive/src/app/logging.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
-// @Injectable({
-//   providedIn: 'root',
-// })
+@Injectable({
+  providedIn: 'root',
+})
 export class Logging {
   log(message: string) {
     const timeStamp = new Date().toLocaleDateString();
--- angular/09-understanding-services-and-dependency-injection-deep-dive/services-and-dependency-injection-deep-dive/src/main.ts
+++ angular/09-understanding-services-and-dependency-injection-deep-dive/services-and-dependency-injection-deep-dive/src/main.ts
@@ -4,4 +4,4 @@ import { App } from './app/app';
 import { TasksService } from './app/tasks/tasks.service';
 
 // bootstrapApplication(App, { providers: [TasksService] }).catch((err) => console.error(err)); // Platform Environment Injection: If service instance is used at a later point in the application, this will make application to bundle the service instance at the time of building and optimization adding an extra burden.
-// bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
