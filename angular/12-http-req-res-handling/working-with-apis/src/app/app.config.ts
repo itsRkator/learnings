2755ec1dfbe67cedf03f30d164c8fe1d9270b9da
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-23 15:53:16 +0530

    Handling HTTP Request and Response Interceptors

--- angular/12-http-req-res-handling/working-with-apis/src/app/app.config.ts
+++ angular/12-http-req-res-handling/working-with-apis/src/app/app.config.ts
@@ -1,14 +1,46 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  // const req = request.clone({
+  //   headers: request.headers.set('X-DEBUG', 'TESTING'),
+  // });
+
+  console.log(`[Outgoing Request]`);
+  console.log(request);
+  // return next(req);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([loggingInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
