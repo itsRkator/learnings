97e6f9049eabf78a83ab0303082f489268240526
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-24 13:15:09 +0530

    Adding form validators (including async validators which can be used to interact with the server

--- angular/13-handling-user-input-and-template-drive-and-reactive-forms/reactive-forms/src/app/auth/login/login.ts
+++ angular/13-handling-user-input-and-template-drive-and-reactive-forms/reactive-forms/src/app/auth/login/login.ts
@@ -1,9 +1,69 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Observable, of } from 'rxjs';
+
+function mustContainDollarSymbol(control: AbstractControl) {
+  if (control.value.includes('$')) {
+    return null;
+  }
+  return { containsDollarSymbol: false };
+}
+
+function isEmailUnique(control: AbstractControl) {
+  if (control.value !== 'rohitashkmwt@gmail.com') {
+    return of(null);
+  }
+
+  return of({ isUnique: false });
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+      asyncValidators: [isEmailUnique],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6), mustContainDollarSymbol],
+    }),
+  });
+
+  get isInvalidEmail() {
+    return (
+      this.loginForm.controls['email'].touched &&
+      this.loginForm.controls['email'].dirty &&
+      this.loginForm.controls['email'].invalid
+    );
+  }
+
+  get isInvalidPassword() {
+    return (
+      this.loginForm.controls['password'].touched &&
+      this.loginForm.controls['password'].dirty &&
+      this.loginForm.controls['password'].invalid
+    );
+  }
+
+  onSubmit() {
+    // Alternate
+    // const enteredEmail = this.loginForm.controls['email'].value;
+    // const enteredPassword = this.loginForm.controls['password'].value;
+
+    const enteredEmail = this.loginForm.value.email;
+    const enteredPassword = this.loginForm.value.password;
+
+    console.log(enteredEmail, enteredPassword);
+  }
+}
