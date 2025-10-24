22794663f2889bcd3db2d261a9af5ea8bb450ec0
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-24 14:42:51 +0530

    Binding and handling forms uisng reactive forms using formGroup

--- angular/13-handling-user-input-and-template-drive-and-reactive-forms/reactive-forms/src/app/app.ts
+++ angular/13-handling-user-input-and-template-drive-and-reactive-forms/reactive-forms/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./auth/login/login";
+import { Signup } from "./auth/signup/signup";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Signup],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- angular/13-handling-user-input-and-template-drive-and-reactive-forms/reactive-forms/src/app/auth/signup/signup.ts
+++ angular/13-handling-user-input-and-template-drive-and-reactive-forms/reactive-forms/src/app/auth/signup/signup.ts
@@ -1,9 +1,55 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
-export class Signup {}
+export class Signup {
+  signUpForm: FormGroup = new FormGroup({
+    email: new FormControl<string>('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl<string>('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    confirmPassword: new FormControl<string>('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    firstName: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+    lastName: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+    street: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+    number: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+    postalCode: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+    city: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {
+      validators: [Validators.required],
+    }),
+    agree: new FormControl<boolean>(false, {
+      validators: [Validators.required],
+    }),
+  });
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+
+  onReset() {
+    console.log('Hello');
+    this.signUpForm.reset();
+  }
+}
