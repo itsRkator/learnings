2a3b52aeeaecfd06e6fac3400ccc9946d5a456e4
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-25 17:32:21 +0530

    Extracting dynamic routes params via inputs with the help of withComponentInputBinding config function in the provideRouter provider

--- angular/14-routing/routing/src/app/users/user-tasks/user-tasks.ts
+++ angular/14-routing/routing/src/app/users/user-tasks/user-tasks.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.html',
   styleUrl: './user-tasks.css',
 })
-export class UserTasks {}
+export class UserTasks implements OnInit {
+  userId = input.required<string>();
+
+  private readonly usersService: UsersService = inject(UsersService);
+
+  userName = computed(() => this.usersService.users.find((u) => u.id === this.userId())?.name);
+
+  ngOnInit(): void {}
+}
