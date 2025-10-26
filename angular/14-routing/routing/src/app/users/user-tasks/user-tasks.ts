d8cfe5434b74cdc62fa06465ad7e8e974bc42b1b
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-26 13:59:14 +0530

    Resolving route-related dynamic data using the resolve and resolver function in the route configuration

--- angular/14-routing/routing/src/app/users/user-tasks/user-tasks.ts
+++ angular/14-routing/routing/src/app/users/user-tasks/user-tasks.ts
@@ -38,9 +38,14 @@ export class UserTasks implements OnInit {
 
   // Different Methods to extract dynamic route params
   // Using signals
-  userId = input.required<string>();
-  userName = computed(() => this.usersService.users.find((u) => u.id === this.userId())?.name);
-  message = input.required<string>();
+  // userId = input.required<string>();
+  // userName = computed(() => this.usersService.users.find((u) => u.id === this.userId())?.name);
+
+  // Using Static Data
+  // message = input.required<string>();
+
+  // Using resolver functions
+  userName = input.required<string>();
 
   // Using Input Decorator
   // selectedUserId: string = '';
@@ -57,7 +62,7 @@ export class UserTasks implements OnInit {
   // userName: string = '';
 
   ngOnInit(): void {
-    console.log(this.message());
+    // console.log(this.message());
     //   console.log(this.activatedRoute.snapshot);
     //   console.log(this.activatedRoute.snapshot.paramMap.get('userId')); // Won't get re-executed as it is just a snapshot not subscription or signal so only once
 
