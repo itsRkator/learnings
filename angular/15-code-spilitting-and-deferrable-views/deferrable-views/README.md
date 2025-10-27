cce76c5be362520be07262fe949c9631460a7f4d
Rohitash Kator <rohitash.kator@talentica.com>
2025-10-27 16:13:40 +0530

    Initial project

--- angular/15-code-spilitting-and-deferrable-views/deferrable-views/README.md
+++ angular/15-code-spilitting-and-deferrable-views/deferrable-views/README.md
@@ -1,4 +1,4 @@
-# LazyLoadingCodeSplitingAndDeferrableViews
+# DeferrableViews
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.4.
 
---
+++ angular/15-code-spilitting-and-deferrable-views/deferrable-views/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Welcome } from './welcome/welcome';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Welcome],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {}
