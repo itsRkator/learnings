562bec3a6ca858284e5a67a931b4da2c46c8a0b4
Rohitash Kator <rohitash.kator@talentica.com>
2025-11-12 15:42:49 +0530

    Working with environment variables via Dockerfile, --env/-e option for key-value pairs and --env-file to map .env file

--- docker/03-managing-data-and-working-with-volumes/data-volumes/index.js
+++ docker/03-managing-data-and-working-with-volumes/data-volumes/index.js
@@ -43,6 +43,6 @@ app.post('/create', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on post 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
